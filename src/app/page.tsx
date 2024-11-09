a7db73a3866ecfc19e6a491a8fb78bf4f3aa70f1
Ali <134097349+AlshehriAli0@users.noreply.github.com>
2024-11-09 16:09:53 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { content } from "@/content";
 
 export default function Home() {
-  return <main className="mx-auto min-h-screen max-w-[75rem]">{content.hero.testing}</main>;
+  return <main className="mx-auto min-h-screen max-w-[85rem]">{content.hero.testing}</main>;
 }
