c37de9c569e4a1ea3a8935dd3476e757119c62ed
Ali <ali0fawzish@outlook.com>
2024-11-13 12:21:17 +0300

    Enhance ArticleCard component image handling for improved responsiveness and layout

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -12,7 +12,13 @@ export const ArticleCard = ({ cover, title, type, href }: ArticleCardProps) => {
   return (
     <div className="flex h-[32rem] w-[23rem] flex-col rounded-3xl bg-[#E4E4E4]">
       <div className="relative h-full w-full">
-        <Image src={cover} alt={cover} layout="fill" objectFit="cover" className="rounded-t-3xl" />
+        <Image
+          sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          src={cover}
+          alt={cover}
+          fill
+          className="rounded-t-3xl object-cover"
+        />
       </div>
       <div className="flex min-h-60 w-full flex-col justify-between rounded-3xl bg-biscay p-5">
         <div className="w-fit rounded-3xl bg-[#E4E4E4] px-2 text-sm font-bold text-biscay">{type}</div>
