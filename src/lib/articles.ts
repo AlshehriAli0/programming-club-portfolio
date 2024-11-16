35086271470faaaf91cf7f75d3ad7a8e08d39a45
Ali <ali0fawzish@outlook.com>
2024-11-16 17:47:01 +0300

    refactor: reorganize post files and update path references

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -17,7 +17,7 @@ interface DateAndSlug {
   slug: string;
 }
 
-const POSTS_PATH = "posts";
+const POSTS_PATH = "src/posts";
 
 export const getRegexForSlug = (slug: string): RegExp => {
   return new RegExp(`^\\d{4}-\\d{2}-\\d{2}-${slug}.mdx$`);
