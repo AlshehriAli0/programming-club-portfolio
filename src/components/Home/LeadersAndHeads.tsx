431694a12a52db38a6ca95a2b3837e304d4cdeb1
Ali <ali0fawzish@outlook.com>
2024-11-27 17:42:26 +0300

    fix: types

--- src/components/Home/LeadersAndHeads.tsx
+++ src/components/Home/LeadersAndHeads.tsx
@@ -1,28 +1,35 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
 
-
-const SingleMember = ({name , role}: any) => {
-  return(
-    <div className='flex flex-col items-center'>
-      <Image src='/Home/Avatar.png' className='w-36 h-36' width={300} height={300} alt='Avatar' />
-      <h1 className='text-black text-[25.05px] font-extrabold'>{name}</h1>
-      <h4 className='text-black text-[23.17px] font-extralight'>{role}</h4>
-    </div>
-  )
-}
-
-export default function LeadersAndHeads({ title , boldTitle , contentOfMember }:any) {
+const SingleMember = ({ name, role }: { name: string; role: string }) => {
   return (
-    <section className='flex flex-col gap-4'>
-      <span className='text-[45.95px] text-breaker-bay flex flex-wrap justify-center text-start'>{title}<h1 className='font-extrabold text-breaker-bay'>{boldTitle}</h1></span>
-      <div className='bg-[#e9eced] flex flex-wrap justify-center gap-x-10 p-6'>
-        {
-          contentOfMember.map((val: { name: string; role: string }, index: number) => {
-            return <SingleMember name={val.name} role={val.role} key={index} />
-          })
-        }
+    <div className="flex flex-col items-center">
+      <Image src="/Home/Avatar.png" className="h-36 w-36" width={300} height={300} alt="Avatar" />
+      <h1 className="text-[25.05px] font-extrabold text-black">{name}</h1>
+      <h4 className="text-[23.17px] font-extralight text-black">{role}</h4>
+    </div>
+  );
+};
+
+export default function LeadersAndHeads({
+  title,
+  boldTitle,
+  contentOfMember,
+}: {
+  title: string;
+  boldTitle: string;
+  contentOfMember: { name: string; role: string }[];
+}) {
+  return (
+    <section className="flex flex-col gap-4">
+      <span className="flex flex-wrap justify-center text-start text-[45.95px] text-breaker-bay">
+        {title}
+        <h1 className="font-extrabold text-breaker-bay">{boldTitle}</h1>
+      </span>
+      <div className="flex flex-wrap justify-center gap-x-10 bg-[#e9eced] p-6">
+        {contentOfMember.map((val: { name: string; role: string }, index: number) => {
+          return <SingleMember name={val.name} role={val.role} key={index} />;
+        })}
       </div>
     </section>
-  )
+  );
 }
--- src/components/Home/MainCard.tsx
+++ src/components/Home/MainCard.tsx
@@ -1,37 +1,41 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { content } from '@/content'
+import { content } from "@/content";
+import { LucideProps } from "lucide-react";
+import React from "react";
 
-const  SingleMaineCard = ({icon, title , desc } : any) => {
-  return(
-    <div className='w-full bg-white flex flex-col sm:flex-row gap-6 items-center rounded-2xl p-8 h-full'>
+const SingleMaineCard = ({
+  icon,
+  title,
+  desc,
+}: {
+  icon: React.ForwardRefExoticComponent<Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>>;
+  title: string;
+  desc: string;
+}) => {
+  return (
+    <div className="flex h-full w-full flex-col items-center gap-6 rounded-2xl bg-white p-8 sm:flex-row">
       {/* Icons */}
-      <div className='text-6xl'>
-        {React.createElement(icon, { className: 'w-24 h-full' })}
-      </div>
+      <div className="text-6xl">{React.createElement(icon, { className: "w-24 h-full" })}</div>
       {/* Text */}
-      <div className='flex flex-col gap-y-4'>
-        <h1 className='text-biscay text-[21.29px] font-semibold'>{title}</h1>
+      <div className="flex flex-col gap-y-4">
+        <h1 className="text-[21.29px] font-semibold text-biscay">{title}</h1>
         <p>{desc}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default function MainCard() {
   return (
-    <section className='flex flex-col gap-y-4 bg-biscay p-28'>
-      <h1 className='text-white text-3xl font-bold'>{content.Home.ProgramUnit.title}</h1>
-      <h2 className='text-white/80 text-2xl font-extralight'>{content.Home.ProgramUnit.desc}</h2>
-      <div className='grid grid-cols-1 sm:grid-cols-2 gap-9 justify-between items-center '>
-        {
-          content.Home.ProgramUnit.ProgramUnitCard.map((val, index) => (
-            <SingleMaineCard key={index} icon={val.logo} title={val.headerOfTitle} desc={val.info} />
-          ))
-        }
+    <section className="flex flex-col gap-y-4 bg-biscay p-28">
+      <h1 className="text-3xl font-bold text-white">{content.Home.ProgramUnit.title}</h1>
+      <h2 className="text-2xl font-extralight text-white/80">{content.Home.ProgramUnit.desc}</h2>
+      <div className="grid grid-cols-1 items-center justify-between gap-9 sm:grid-cols-2">
+        {content.Home.ProgramUnit.ProgramUnitCard.map((val, index) => (
+          <SingleMaineCard key={index} icon={val.logo} title={val.headerOfTitle} desc={val.info} />
+        ))}
       </div>
     </section>
-  )
+  );
 }
-
