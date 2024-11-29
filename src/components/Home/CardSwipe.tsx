b4a93db0ecce2432cf16dcfbd04950088a3fbe81
Ali <ali0fawzish@outlook.com>
2024-11-29 12:11:57 +0300

    fix: build issues

--- src/components/Home/CardSwipe.tsx
+++ src/components/Home/CardSwipe.tsx
@@ -1,77 +1,77 @@
-'use client'
+"use client";
 
-
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import SingleCard from './SingleCard';
-import { Mail } from 'lucide-react';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import SingleCard from "./SingleCard";
 
 export default function CardSwipe() {
   return (
-    <section className='flex flex-col items-center gap-y-28 px-5'>
-        <p className='text-center text-[#174760] sm:text-[37.86px] text-2xl font-normal'>a creative and supportive community to develop our skills and learn together outside the academic scope</p>
+    <section className="flex flex-col items-center gap-y-28 px-5">
+      <p className="text-center text-2xl font-normal text-[#174760] sm:text-[37.86px]">
+        a creative and supportive community to develop our skills and learn together outside the academic
+        scope
+      </p>
 
-        <Carousel
-          additionalTransfrom={0}
-          arrows
-          autoPlaySpeed={3000}
-          centerMode={false}
-          className="border-biscay border-2 rounded-2xl"
-          containerClass="container"
-          dotListClass=""
-          draggable
-          focusOnSelect
-          infinite={false}
-          itemClass=""
-          keyBoardControl
-          minimumTouchDrag={80}
-          partialVisible
-          pauseOnHover
-          renderArrowsWhenDisabled={false}
-          renderButtonGroupOutside={false}
-          renderDotsOutside={false}
-          responsive={{
-            desktop: {
-              breakpoint: {
-                max: 3000,
-                min: 1024
-              },
-              items: 1,
-              partialVisibilityGutter: 40
+      <Carousel
+        additionalTransfrom={0}
+        arrows
+        autoPlaySpeed={3000}
+        centerMode={false}
+        className="rounded-2xl border-2 border-biscay"
+        containerClass="container"
+        dotListClass=""
+        draggable
+        focusOnSelect
+        infinite={false}
+        itemClass=""
+        keyBoardControl
+        minimumTouchDrag={80}
+        partialVisible
+        pauseOnHover
+        renderArrowsWhenDisabled={false}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={false}
+        responsive={{
+          desktop: {
+            breakpoint: {
+              max: 3000,
+              min: 1024,
             },
-            mobile: {
-              breakpoint: {
-                max: 464,
-                min: 0
-              },
-              items: 1,
-              partialVisibilityGutter: 30
+            items: 1,
+            partialVisibilityGutter: 40,
+          },
+          mobile: {
+            breakpoint: {
+              max: 464,
+              min: 0,
             },
-            tablet: {
-              breakpoint: {
-                max: 1024,
-                min: 464
-              },
-              items: 1,
-              partialVisibilityGutter: 30
-            }
-          }}
-          rewind={false}
-          rewindWithAnimation={false}
-          shouldResetAutoplay
-          showDots
-          sliderClass=""
-          slidesToSlide={1}
-          swipeable
-          >
-            <SingleCard />
-            <SingleCard />
-            <SingleCard />
-            <SingleCard />
-            <SingleCard />
-            <SingleCard />
-        </Carousel>
-</section>
-
-  )
+            items: 1,
+            partialVisibilityGutter: 30,
+          },
+          tablet: {
+            breakpoint: {
+              max: 1024,
+              min: 464,
+            },
+            items: 1,
+            partialVisibilityGutter: 30,
+          },
+        }}
+        rewind={false}
+        rewindWithAnimation={false}
+        shouldResetAutoplay
+        showDots
+        sliderClass=""
+        slidesToSlide={1}
+        swipeable
+      >
+        <SingleCard />
+        <SingleCard />
+        <SingleCard />
+        <SingleCard />
+        <SingleCard />
+        <SingleCard />
+      </Carousel>
+    </section>
+  );
 }
--- src/components/Home/LeadersAndHeads.tsx
+++ src/components/Home/LeadersAndHeads.tsx
@@ -1,28 +1,41 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
 
-
-const SingleMember = ({name , role}: { name: string; role: string }) => {
-  return(
-    <div className='flex flex-col items-center text-center'>
-      <Image src='/Home/Avatar.png' className='sm:w-36 w-28 sm:h-36 h-28' width={300} height={300} alt='Avatar' />
-      <h1 className='text-black sm:text-[25.05px] text-2xl font-extrabold'>{name}</h1>
-      <h4 className='text-black text-[23.17px] font-extralight'>{role}</h4>
-    </div>
-  )
-}
-
-export default function LeadersAndHeads({ title , boldTitle , contentOfMember }:any) {
+const SingleMember = ({ name, role }: { name: string; role: string }) => {
   return (
-    <section className='flex flex-col gap-6'>
-      <span className='sm:text-[45.95px] text-3xl text-breaker-bay flex flex-wrap gap-x-2 justify-center text-start'>{title}<h1 className='font-extrabold text-breaker-bay'>{boldTitle}</h1></span>
-      <div className='bg-[#e9eced] flex flex-wrap justify-center gap-10 p-6'>
-        {
-          contentOfMember.map((val: { name: string; role: string }, index: number) => {
-            return <SingleMember name={val.name} role={val.role} key={index} />
-          })
-        }
+    <div className="flex flex-col items-center text-center">
+      <Image
+        src="/Home/Avatar.png"
+        className="h-28 w-28 sm:h-36 sm:w-36"
+        width={300}
+        height={300}
+        alt="Avatar"
+      />
+      <h1 className="text-2xl font-extrabold text-black sm:text-[25.05px]">{name}</h1>
+      <h4 className="text-[23.17px] font-extralight text-black">{role}</h4>
+    </div>
+  );
+};
+
+export default function LeadersAndHeads({
+  title,
+  boldTitle,
+  contentOfMember,
+}: {
+  title: string;
+  boldTitle: string;
+  contentOfMember: { name: string; role: string }[];
+}) {
+  return (
+    <section className="flex flex-col gap-6">
+      <span className="flex flex-wrap justify-center gap-x-2 text-start text-3xl text-breaker-bay sm:text-[45.95px]">
+        {title}
+        <h1 className="font-extrabold text-breaker-bay">{boldTitle}</h1>
+      </span>
+      <div className="flex flex-wrap justify-center gap-10 bg-[#e9eced] p-6">
+        {contentOfMember.map((val: { name: string; role: string }, index: number) => {
+          return <SingleMember name={val.name} role={val.role} key={index} />;
+        })}
       </div>
     </section>
-  )
+  );
 }
